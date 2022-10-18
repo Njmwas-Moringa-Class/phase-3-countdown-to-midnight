f2c3074856177f6660fc763ab79414569fb9f779
Julius Mwangi <julius.mwangi@moringaschool.com>
2022-10-18 10:19:34 +0300

    commit tree fed285a262d9fb1b369cf6896db8dbeef7c9f40d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+require "pry"
+def countdown counter
+    while counter > 0 do
+        puts "#{counter} SECOND(S)!"
+        counter -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep number
+    while number > 0 do
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+end
+
+# binding.pry
